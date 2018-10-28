2c6fe564078c9a18453db16893270da5d8ee22e2
cheferrari <34338482+cheferrari@users.noreply.github.com>
2018-10-29 00:04:09 +0800

    Update entrypoint.sh

--- containerization-spring-with-helm/entrypoint.sh
+++ containerization-spring-with-helm/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 ACTIVE_PROFILE=${PROFILE:=default}
-java -Xmx1024m -Djava.security.egd=file:/dev/./urandom -jar gs-spring-boot.jar --spring.profiles.active=${ACTIVE_PROFILE} $@
+java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar gs-spring-boot.jar --spring.profiles.active=${ACTIVE_PROFILE} $@
